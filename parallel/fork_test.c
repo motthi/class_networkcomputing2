d39c88111a9dfac9193512ac4df4645090e0700e
Pasises <45708319+pasises@users.noreply.github.com>
2020-06-14 01:27:57 +0900

    feat : improved_fork

--- parallel/fork_test.c
+++ parallel/fork_test.c
@@ -12,16 +12,30 @@ void signal_handler(int sig);
 int main() {
 	int listen_sock, accept_sock, pid;
 	socklen_t sin_siz;
+	struct sockaddr_in addr;
 	struct sockaddr_in serv, clt;
 	unsigned short port;
+	int yes = 1;
 
+	printf("Now Setting...\n");
 	signal(SIGCHLD, signal_handler);
 
+	listen_sock			 = socket(AF_INET, SOCK_STREAM, 0);
+	addr.sin_family		 = AF_INET;
+	addr.sin_port		 = htons(22629);
+	addr.sin_addr.s_addr = INADDR_ANY;
+
+	setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, (const char*)&yes, sizeof(yes));		  //TIME_WAIT状態でも再起動可能に設定
+	if(bind(listen_sock, (struct sockaddr*)&addr, sizeof(addr)) == -1) {					  //通信元の情報を与える
+		perror("bind");
+		return 0;
+	}
 	if((listen_sock = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
 		perror("socket");
 		exit(1);
 	}
 
+	printf("Ready to Start\n");
 	while(1) {
 		accept_sock = accept(listen_sock, (struct sockaddr*)&clt, &sin_siz);
 		pid			= fork();
