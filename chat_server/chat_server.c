71d084a4955195517679a8315b413d442c2d5ec3
Pasises <45708319+pasises@users.noreply.github.com>
2020-07-23 17:59:19 +0900

    feat : check

--- chat_server/chat_server.c
+++ chat_server/chat_server.c
@@ -93,7 +93,7 @@ int main(int argc, char* argv[]) {
 			} else {
 				int connect_d = events[i].data.fd;
 				read_line(connect_d, buf, sizeof(buf));
-				printf("ccc\t %s", buf);
+				printf("ccc\t %s\tbuf finesh", buf);
 
 				if(strncmp(buf, ":q", 2) == 0) {		//終了コマンド
 					/* クライアントを切断する */
